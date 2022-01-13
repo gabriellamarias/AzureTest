ebe90d14a0246a204383665fa74e8e392c0f1029
gabi <gabriella.marias@gmail.com>
2022-01-12 21:54:38 -0500

    I deleted deleting and weatherapp

--- WebApplication5/Controllers/WeatherForecastController.cs
+++ WebApplication5/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApplication5.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Very cold", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
